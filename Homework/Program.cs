91dc3819d1e842891096fd26bcfbd09c1a8ead72
Olena <49119089+HOlena@users.noreply.github.com>
2021-05-18 17:36:59 +0300

    project init

---
+++ Homework/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] map =
+            {
+            {' ',' ',' ',' ',' ',' ','*',' ',' ',' ',' ',' ',' ' },
+            {' ',' ',' ',' ',' ','*','*','*',' ',' ',' ',' ',' ' },
+            {' ',' ',' ',' ','*','*','*','*','*',' ',' ',' ',' ' },
+            {' ',' ',' ','*','*','*','*','*','*','*',' ',' ',' ' },
+            {' ',' ','*','*','*','*','*','*','*','*','*',' ',' ' },
+            {' ','*','*','*','*','*','*','*','*','*','*','*',' ' },
+            {'*','*','*','*','*','*','*','*','*','*','*','*','*'}
+            };
+
+            while (true)
+            {
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        Console.Write(map[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
